7d6a3a51745ea0fac0ac94e1c90dd1320b42be75
Skip Khamvongsa <skipk95@gmail.com>
2020-08-01 13:24:32 -0600

    Added new resume, edited portfolio footer, and added description of pdf on home

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,30 @@
-import emailjs from 'emailjs-com'
-import apiKeys from './apikeys'
+// import emailjs from 'emailjs-com'
+// import apiKeys from './apikeys'
+
 
 const sendEmail = e => {
-    e.preventDefault()
- 
-    emailjs
-    .sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
-    .then(
-      result => {
-        console.log(result.text)
-      },
-      error => {
-        console.log(error.text)
-      }
-    )
+  e.preventDefault()
+  var template_params = {
+    "name": formdata.name,
+    "email": formdata.email,
+    "notes": formdata.message
+  }
+  var service_id = "gmail";
+  var template_id = "contact_page";
+  window.emailjs.send(service_id, template_id, template_params);
+  // setMessage('Your message has been sent!!!');
+  
+  // emailjs
+  //   .sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+  //   .then(
+  //     result => {
+  //       console.log(result.text)
+  //     },
+  //     error => {
+  //       console.log(error.text)
+  //     }
+  //   )
 }
 
 const form = document.querySelector('.form')
-form.addEventListener('submit',sendEmail)
+form.addEventListener('submit', sendEmail)
